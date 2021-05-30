7c166c0c0a303f5b00520df21edbdeee055faba0
natcassidy <nat.cassidy865@gmail.com>
2021-05-30 14:01:22 -0300

    first commit

---
+++ src/main/java/com/example/demobugtracker/DemoBugtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobugtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBugtrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBugtrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demobugtracker/DemoBugtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demobugtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBugtrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
